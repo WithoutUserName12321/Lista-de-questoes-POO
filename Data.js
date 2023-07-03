75101462e4fc5988c31fec61104e5f151fe2c61e
Sthefania Ferreira <aniafehts@gmail.com>
2023-07-03 17:08:27 -0300

    verificação de validade das datas inseridas

--- Data.js
+++ Data.js
@@ -1,7 +1,20 @@
-module.exports = class Data{
-    constructor(dia,mes,ano){
+module.exports = class Data {
+    constructor(dia, mes, ano) {
         this.Dia = dia;
         this.Mes = mes;
         this.Ano = ano;
+
+        if (this.mes < 1 || this.mes > 12) {
+            return false;
+        }
+
+        if (this.dia < 1 || this.dia > 31) {
+            return false;
+        }
+        
+        if (this.ano <= 0) {
+            return false;
+        }
+        return true;
     }
 }
