6b0ceb8214a03aa264acb44f81af08406ccf431e
Sthefania Ferreira <aniafehts@gmail.com>
2023-07-03 15:09:53 -0300

    Criação da classe "Aluno"

---
+++ Aluno.js
@@ -0,0 +1,10 @@
+module.exports = class Aluno {
+    constructor(mat, nome, n1, n2, pt) {
+        this.matricula = mat
+        this.nome = nome
+        this.nota1 = n1
+        this.nota2 = n2
+        this.trabalho = pt
+        this.media
+    }
+}
